1019cf3d7af75dc5e20afa958ecec3d01015e7c2
zuo <2105065907@qq.com>
2024-08-11 15:01:55 +0800

    修改readme

--- README.md
+++ README.md
@@ -2,13 +2,37 @@
 
 ### 前端环境
 
-nodeJS:v18.16.0   npm:9.5.1  vue3、vue-cli脚手架  arco-design框架
+nodeJS:v18.16.0   npm:9.5.1  vue3、vue-cli脚手架 Element框架
 
 ### 后端环境
 
 jdk： 1.8   springboot:2.7
 
+### 技术选型
 
+前端： Vue3+Element+Vuex+ByteMarkDown+Monaco-editer
+
+后端：springboot+RabbitMq+虚拟机+Docker
+
+### 功能模块
+
+- 用户模块
+  - 注册
+  - 登录
+- 题目模块
+  - 创建题目
+  - 删除题目
+  - 修改题目
+  - 查询题目
+  - 在线做题
+  - 查看题目提交结果
+
+- 判题模块
+  - 代码沙箱自动运行用户提交代码
+  - 自动判断用户提交代码运行结果
+  - 错误处理
+
+### 代码沙箱地址 ：[onlyup0/code-sandbox (github.com)](https://github.com/onlyup0/code-sandbox)
 
 ### 运行截图
 
---
+++ zojWeb/zoj/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="homeView">
+    <a-typography style="width: 60%; margin: 0 auto">
+      <a-typography-title> 注意： </a-typography-title>
+      <a-typography-paragraph>
+        提交题目请严格按照以下格式提交：
+      </a-typography-paragraph>
+      <a-typography-paragraph>
+        <a-typography-text mark>类名</a-typography-text
+        >为Main,<a-typography-text mark>输入</a-typography-text
+        >在main方法中输入，<a-typography-text mark>输出</a-typography-text
+        >为System.out.print()
+      </a-typography-paragraph>
+      <a-typography-title :heading="2">例题：a+b</a-typography-title>
+      <a-typography-paragraph>
+        <MdEditor :value="example" :readonly="true" />
+      </a-typography-paragraph>
+    </a-typography>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MdViewer from "@/components/MdViewer.vue";
+import MdEditor from "@/components/MdEditor.vue";
+import { ref } from "vue";
+
+const example = ref(
+  "public class Main {\npublic static void main(String[] args) {\nint a = Integer.parseInt(args[0]);\nint b = Integer.parseInt(args[1]);\nSystem.out.print((a + b));}\n}"
+);
+</script>
+<style></style>
